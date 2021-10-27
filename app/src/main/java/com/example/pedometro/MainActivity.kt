895d2d40223c87bb48331b1f9d390e04bfae8ad7
Rave7v <fernando080499@hotmail.com>
2021-10-26 21:10:35 -0500

    modificaciones hechas en clase

--- app/src/main/java/com/example/pedometro/MainActivity.kt
+++ app/src/main/java/com/example/pedometro/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.pedometro
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -31,12 +32,15 @@ class MainActivity : AppCompatActivity() {
 
         val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         val sensorPasos: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR)
+        val giros : Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
         Log.d("SensorExamples",sensorPasos.toString())
+        Log.d("SensorExamples",giros.toString())
 
         var pasos: Float= 0.0F
-        val sensorEventListener : SensorEventListener = object : SensorEventListener{
+        var girando : Float=0.0F
+        val sensorEventListener : SensorEventListener = object : SensorEventListener {
             override fun onSensorChanged(sensorEvent: SensorEvent) {
-                for(value in sensorEvent.values){
+                for (value in sensorEvent.values) {
                     pasos += value
                 }
                 binding.cuentapasos.setText("Pasos:  $pasos")
@@ -47,12 +51,23 @@ class MainActivity : AppCompatActivity() {
                 //TODO("Not yet implemented")
             }
         }
+        val sensorEventListener1: SensorEventListener = object : SensorEventListener{
+            override fun onSensorChanged(sensorEvent1: SensorEvent) {
+                binding.giros.setText("Giro: ${sensorEvent1.values[0]}, ${sensorEvent1.values[1]}, ${sensorEvent1.values[2]}")
+                if(sensorEvent1.values[0]>0.5F){
+                    binding.colorsito.setBackgroundColor(Color.BLACK)
+                }
+                if(sensorEvent1.values[0]< -0.5F){
+                    binding.colorsito.setBackgroundColor(Color.CYAN)
+                }
+            }
+
+            override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+                //TODO("Not yet implemented")
+            }
+        }
 
         sensorManager.registerListener(sensorEventListener, sensorPasos, SensorManager.SENSOR_DELAY_FASTEST)
-
+        sensorManager.registerListener(sensorEventListener1, giros, SensorManager.SENSOR_DELAY_FASTEST)
     }
-
-
-
-
 }
